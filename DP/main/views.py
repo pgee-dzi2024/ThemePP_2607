ec31684d94ebf3636fc5f788c1dae2de6e180687
pgee-dzi2024 <pgee_dzi2024@abv.bg>
2026-03-09 10:41:48 +0200

    Малки промени по дизайна

--- DP/main/views.py
+++ DP/main/views.py
@@ -1,4 +1,15 @@
+import pandas as pd
 from django.shortcuts import render
 
 def index(request):
+    if request.method == "POST" and request.FILES.get("datafile"):
+        file = request.FILES["datafile"]
+
+        if file.name.endswith(".csv"):
+            df = pd.read_csv(file)
+
+        elif file.name.endswith(".xlsx"):
+            df = pd.read_excel(file)
+
+        print(df.head())
     return render(request, 'main/index.html')
