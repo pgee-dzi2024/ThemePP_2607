8619d1d52cc300638a5788d8de34d6b807e6d932
pgee-dzi2024 <pgee_dzi2024@abv.bg>
2026-03-13 11:36:50 +0200

    Промени по дизайна

--- DP/main/urls.py
+++ DP/main/urls.py
@@ -6,6 +6,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('', index, name='home'),
+    path('about/', about, name='about'),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- DP/main/views.py
+++ DP/main/views.py
@@ -2,7 +2,9 @@ import pandas as pd
 from django.shortcuts import render
 
 def index(request):
+
     if request.method == "POST" and request.FILES.get("datafile"):
+
         file = request.FILES["datafile"]
 
         if file.name.endswith(".csv"):
@@ -12,4 +14,9 @@ def index(request):
             df = pd.read_excel(file)
 
         print(df.head())
+
     return render(request, 'main/index.html')
+
+
+def about(request):
+    return render(request, 'main/about.html')
