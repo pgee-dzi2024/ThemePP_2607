9a9c07cd863f5c37fd6a2ab5ba334a9eda005fcc
gimas-bansko <63417802+gimas-bansko@users.noreply.github.com>
2026-04-12 16:46:17 +0300

    ФИНАЛ

--- downloads/DB/dzi_pp_2607.sql
+++ downloads/DB/dzi_pp_2607.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Хост: 127.0.0.1
--- Време на генериране: 10 фев 2026 в 02:14
+-- Време на генериране: 12 апр 2026 в 15:44
 -- Версия на сървъра: 10.4.32-MariaDB
 -- Версия на PHP: 8.2.12
 
@@ -85,7 +85,11 @@ INSERT INTO `auth_permission` (`id`, `name`, `content_type_id`, `codename`) VALU
 (21, 'Can add session', 6, 'add_session'),
 (22, 'Can change session', 6, 'change_session'),
 (23, 'Can delete session', 6, 'delete_session'),
-(24, 'Can view session', 6, 'view_session');
+(24, 'Can view session', 6, 'view_session'),
+(25, 'Can add analysis', 7, 'add_analysis'),
+(26, 'Can change analysis', 7, 'change_analysis'),
+(27, 'Can delete analysis', 7, 'delete_analysis'),
+(28, 'Can view analysis', 7, 'view_analysis');
 
 -- --------------------------------------------------------
 
@@ -112,7 +116,7 @@ CREATE TABLE `auth_user` (
 --
 
 INSERT INTO `auth_user` (`id`, `password`, `last_login`, `is_superuser`, `username`, `first_name`, `last_name`, `email`, `is_staff`, `is_active`, `date_joined`) VALUES
-(1, 'pbkdf2_sha256$600000$wa69HjK5kgC4b6c8omaGHs$cFMDie+9dPx9Zzimr9sZcn8MPjuBs0S36p3XrbIwdNQ=', NULL, 1, 'user_26', '', '', '', 1, 1, '2026-02-10 00:57:22.892428');
+(1, 'pbkdf2_sha256$600000$wa69HjK5kgC4b6c8omaGHs$cFMDie+9dPx9Zzimr9sZcn8MPjuBs0S36p3XrbIwdNQ=', '2026-04-12 12:29:33.975786', 1, 'user_26', '', '', '', 1, 1, '2026-02-10 00:57:22.892428');
 
 -- --------------------------------------------------------
 
@@ -177,6 +181,7 @@ INSERT INTO `django_content_type` (`id`, `app_label`, `model`) VALUES
 (2, 'auth', 'permission'),
 (4, 'auth', 'user'),
 (5, 'contenttypes', 'contenttype'),
+(7, 'main', 'analysis'),
 (6, 'sessions', 'session');
 
 -- --------------------------------------------------------
@@ -214,7 +219,8 @@ INSERT INTO `django_migrations` (`id`, `app`, `name`, `applied`) VALUES
 (15, 'auth', '0010_alter_group_name_max_length', '2026-02-10 00:56:44.338455'),
 (16, 'auth', '0011_update_proxy_permissions', '2026-02-10 00:56:44.345887'),
 (17, 'auth', '0012_alter_user_first_name_max_length', '2026-02-10 00:56:44.356375'),
-(18, 'sessions', '0001_initial', '2026-02-10 00:56:44.384878');
+(18, 'sessions', '0001_initial', '2026-02-10 00:56:44.384878'),
+(19, 'main', '0001_initial', '2026-04-11 23:50:47.613720');
 
 -- --------------------------------------------------------
 
@@ -227,6 +233,66 @@ CREATE TABLE `django_session` (
   `session_data` longtext NOT NULL,
   `expire_date` datetime(6) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Схема на данните от таблица `django_session`
+--
+
+INSERT INTO `django_session` (`session_key`, `session_data`, `expire_date`) VALUES
+('q0uuo2sf2s4qghnhlt9jniy5t6rhc5fe', '.eJxVjMEKwyAQRP_FcxHNumJ67L3fIKurNW0xEJNT6L83Qg7taWDem9mFp20tfmtp8ROLq9Di8tsFiq9UO-An1ccs41zXZQqyK_KkTd5nTu_b6f4dFGrlWEftbGY1Eg8GEhplGE3KwNrkbCFYBmTSw6gihODQaYLYsT6CEMXnC-k3OB0:1wBtpO:_fgQkdTOQVoA-gPkxxNh5V0lgTdutwbz60sMaGsCL0A', '2026-04-26 12:22:14.595079'),
+('s2tumyw0ve9eoqrp39qcqi3noy6ngj80', '.eJydUMtOxDAM_JeeUZVnH3tEe-Gw0h44oUiREzu00JeaFK2E-HdSdpFA3DhZ9oxnxn4vLGyps1uk1fZYHApe3P2cOfCvNO0AvsD0PJd-ntLau3KnlDc0lqcZabi_cX8JdBC7vO15UwVkLaBQkrRiCrWiIJGrECrpKpQagYuWeelcoxsO0u8wzwW0zqLbMsyAhDb0A9kF0q57PBhzfHqwgonKmPPZmMeORrKstid47jOYRyNhD8ZcBaIxdbZG3mrwKJXw4HTg2ZY1lcDacVdehnj54zjBSPsl87hsidZoE8VkM_WbPkDu6bLMa7I-vv0n4nU7R_z6HbdN0FrUIihFXOWsrq65bpQCEVpWYVtmn-LjE5M7l5E:1wBuvs:OGvxYTgfcEhDiGbbWQdv9gRQsu7szWr0q1GqR3bZUIU', '2026-04-26 13:33:00.405393');
+
+-- --------------------------------------------------------
+
+--
+-- Структура на таблица `main_analysis`
+--
+
+CREATE TABLE `main_analysis` (
+  `id` bigint(20) NOT NULL,
+  `filename` varchar(255) NOT NULL,
+  `total` double NOT NULL,
+  `average` double NOT NULL,
+  `created_at` datetime(6) NOT NULL,
+  `user_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Схема на данните от таблица `main_analysis`
+--
+
+INSERT INTO `main_analysis` (`id`, `filename`, `total`, `average`, `created_at`, `user_id`) VALUES
+(1, 'products_csv.csv', 610, 122, '2026-04-12 09:54:56.748683', 1),
+(2, 'products_csv.csv', 610, 122, '2026-04-12 09:56:08.432400', 1),
+(3, 'dates_data_csv.csv', 3355, 167.75, '2026-04-12 09:56:16.845688', 1),
+(4, 'computers_test_xls.xlsx', 720, 120, '2026-04-12 09:57:32.972622', 1),
+(5, 'computers_test_xls.xlsx', 720, 120, '2026-04-12 09:57:46.743346', 1),
+(6, 'computers_test_xls.xlsx', 720, 120, '2026-04-12 10:13:58.740022', 1),
+(7, 'computers_test_xls.xlsx', 720, 120, '2026-04-12 10:46:06.690345', 1),
+(8, 'expenses_data_xls.xlsx', 5300, 1060, '2026-04-12 10:50:00.924847', 1),
+(9, 'students_data_xls.xlsx', 26, 5.2, '2026-04-12 10:50:23.819950', 1),
+(10, 'laptops_test_xls.xlsx', 670, 111.67, '2026-04-12 10:50:39.349122', 1),
+(11, 'monthly_sales_csv.csv', 3050, 254.17, '2026-04-12 10:51:11.712576', 1),
+(12, 'monthly_sales_csv.csv', 3050, 254.17, '2026-04-12 11:10:46.437232', 1),
+(13, 'monthly_sales_csv.csv', 3050, 3050, '2026-04-12 11:11:26.108747', 1),
+(14, 'monthly_sales_csv.csv', 3050, 254.17, '2026-04-12 11:15:16.875708', 1),
+(15, 'monthly_sales_csv.csv', 3050, 254.17, '2026-04-12 11:21:28.779087', 1),
+(16, 'sales_data_xls.xlsx', 3810, 635, '2026-04-12 11:21:47.924584', 1),
+(17, 'sales_data_xls.xlsx', 3810, 635, '2026-04-12 11:22:26.676114', 1),
+(18, 'sales_data_xls.xlsx', 3810, 635, '2026-04-12 11:22:41.703416', 1),
+(19, 'sales_data_xls.xlsx', 3810, 635, '2026-04-12 11:22:57.883302', 1),
+(20, 'computers_test_xls.xlsx', 720, 120, '2026-04-12 11:27:09.812877', 1),
+(21, 'computers_test_xls.xlsx', 720, 120, '2026-04-12 11:27:39.128987', 1),
+(22, 'computers_test_xls.xlsx', 720, 120, '2026-04-12 11:27:59.210455', 1),
+(23, 'computers_test_xls.xlsx', 720, 120, '2026-04-12 11:34:40.675879', 1),
+(24, 'computers_test_xls.xlsx', 720, 120, '2026-04-12 11:38:23.574104', 1),
+(25, 'computers_test_xls.xlsx', 720, 120, '2026-04-12 11:38:32.870341', 1),
+(26, 'computers_test_xls.xlsx', 720, 120, '2026-04-12 12:16:57.474772', 1),
+(27, 'computers_test_xls.xlsx', 720, 120, '2026-04-12 12:19:12.062194', 1),
+(28, 'computers_test_xls.xlsx', 720, 120, '2026-04-12 12:19:54.565641', 1),
+(29, 'computers_test_xls.xlsx', 720, 120, '2026-04-12 12:22:09.430274', 1),
+(30, 'computers_test_xls.xlsx', 720, 120, '2026-04-12 13:27:52.902781', 1),
+(31, 'computers_test_xls.xlsx', 720, 120, '2026-04-12 13:33:00.394882', 1);
 
 --
 -- Indexes for dumped tables
@@ -306,6 +372,13 @@ ALTER TABLE `django_session`
   ADD KEY `django_session_expire_date_a5c62663` (`expire_date`);
 
 --
+-- Индекси за таблица `main_analysis`
+--
+ALTER TABLE `main_analysis`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `main_analysis_user_id_cff8ced5_fk_auth_user_id` (`user_id`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -325,7 +398,7 @@ ALTER TABLE `auth_group_permissions`
 -- AUTO_INCREMENT for table `auth_permission`
 --
 ALTER TABLE `auth_permission`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=25;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=29;
 
 --
 -- AUTO_INCREMENT for table `auth_user`
@@ -355,13 +428,19 @@ ALTER TABLE `django_admin_log`
 -- AUTO_INCREMENT for table `django_content_type`
 --
 ALTER TABLE `django_content_type`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT for table `django_migrations`
 --
 ALTER TABLE `django_migrations`
-  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
+
+--
+-- AUTO_INCREMENT for table `main_analysis`
+--
+ALTER TABLE `main_analysis`
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=32;
 
 --
 -- Ограничения за дъмпнати таблици
@@ -400,6 +479,12 @@ ALTER TABLE `auth_user_user_permissions`
 ALTER TABLE `django_admin_log`
   ADD CONSTRAINT `django_admin_log_content_type_id_c4bce8eb_fk_django_co` FOREIGN KEY (`content_type_id`) REFERENCES `django_content_type` (`id`),
   ADD CONSTRAINT `django_admin_log_user_id_c564eba6_fk_auth_user_id` FOREIGN KEY (`user_id`) REFERENCES `auth_user` (`id`);
+
+--
+-- Ограничения за таблица `main_analysis`
+--
+ALTER TABLE `main_analysis`
+  ADD CONSTRAINT `main_analysis_user_id_cff8ced5_fk_auth_user_id` FOREIGN KEY (`user_id`) REFERENCES `auth_user` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
